bf3fee91bb3c6e15915f3da8018c0de47631cf0e
Nitesh Kr Das <niteshkrdas12@gmail.com>
2023-07-14 00:13:03 +0545

    darkmode switch added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 function App() {
+  const [mode, setMode]=useState("light")
+
+  const toggleMode=()=>{
+    if(mode=== 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor="gray";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="white";
+
+    }
+  }
+
   return (
     <>
-    <Navbar title="Home" aboutText="About"/>
+    <Navbar title="Home" aboutText="About" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
     <Textform heading="Enter the text to analyze"/>
     </div>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -8,23 +8,35 @@ export default function Textform(props) {
         let newText=text.toUpperCase();
         setText(newText)
     }
-
+    const handlelowerClick=()=>{
+        // console.log("upper case clicked");
+         let newText=text.toLowerCase();
+         setText(newText)
+     }
     const handleOnChange=(event)=>{
        // console.log("on change");
         setText(event.target.value);
     }
 
-    const[text,setText]=useState("Enter text here");
+    const[text,setText]=useState("");
 
     return (
-    <div>
+        <>
+        <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <div className="input-group">
            <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         </div>
         </div>
-        <button className='btn btn-primary' onClick={handleUpClick}>convert to uppercase</button>
-    </div>
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>convert to uppercase</button>
+        <button className='btn btn-primary mx-4' onClick={handlelowerClick}>convert to lowercase</button>
+        </div>
+        <div className='container my-3'>
+        Text Summary
+        <p>{text.split(" ").length}words and {text.length}Characters</p>
+        </div>
+
+        </>
   )
 }
